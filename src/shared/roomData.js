e21e2786e5d04a9fe4a2ab8ee78f8df5c5343b42
itissandeep98 <itissandeep98@gmail.com>
2020-08-11 17:30:53 +0530

    fonts updated

--- src/shared/roomData.js
+++ src/shared/roomData.js
@@ -58,5 +58,20 @@ export const room=[
 		"type": "Family Room",
 		"num": 104,
 		"detail": "Family Room | 4 Beds |"
+	},
+	{
+		"type": "Deluxe Room",
+		"num": 101,
+		"detail": "1 Bedroom"
+	},
+	{
+		"type": "King Room",
+		"num": 102,
+		"detail": "2 King Size Bed Rooms"
+	},
+	{
+		"type": "Family Room",
+		"num": 104,
+		"detail": "Family Room | 4 Beds |"
 	}
 ]
