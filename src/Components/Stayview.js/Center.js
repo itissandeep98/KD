5761162db11e90d7069583e38baf5492736ba6d1
itissandeep98 <itissandeep98@gmail.com>
2020-08-11 17:21:55 +0530

    center table added

--- src/Components/Stayview.js/Center.js
+++ src/Components/Stayview.js/Center.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import { Table } from 'reactstrap'
 
 export default class Center extends Component {
 	render() {
+		const head=[]
+		const body=[]
+		for (let i = 8; i < 17; i++) {
+			var element = <td>{i}</td>
+			head.push(element)			
+		}
+		for (let i = 0; i < 50; i++) {
+			const element=[]
+			for (let j = 0; j < 9; j++) {
+				element.push(<td/>)
+			}
+			body.push(<tr>{element}</tr>)
+		}
 		return (
 			<div className="col-6 col-lg-8 text-center border tabs">
-				Center
+				<Table bordered>
+					<thead>
+						<tr>
+							<td colSpan={9}>
+								Mon 01 Dec
+							</td>
+						</tr>
+					</thead>
+					<thead>
+						<tr>
+							{head}
+						</tr>
+					</thead>
+					<tbody>
+						{body}
+					</tbody>
+
+				</Table>
 			</div>
 		)
 	}
