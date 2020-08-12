3aba5f5b8930b82a8fe9d7b4f30b4cfa4f933694
itissandeep98 <itissandeep98@gmail.com>
2020-08-12 22:53:26 +0530

    form ui update

--- src/Components/Stayview.js/NewEventCard.js
+++ src/Components/Stayview.js/NewEventCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Card, CardBody, Button, CardHeader, Form, FormGroup, Label, Input, Row, Col, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, PopoverBody, UncontrolledPopover } from 'reactstrap'
+import { Card, CardBody, Button, CardHeader, Form, FormGroup, Label, Input, Row, Col, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, PopoverBody, UncontrolledPopover, InputGroup, InputGroupAddon } from 'reactstrap'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 
-function NewEventCard ({cancel}) {
+function NewEventCard({ cancel }) {
 	return (
 		<div>
 			<Card id="popupcard">
@@ -12,12 +12,12 @@ function NewEventCard ({cancel}) {
 					<Form>
 						<FormGroup className="row">
 							<Label className="col-3"> Name</Label>
-							<Input className="col-8" type="text"/>
+							<Input className="col-8" type="text" />
 						</FormGroup>
 						<FormGroup className="row">
 							<Label className="col-3"> Resource</Label>
 							<UncontrolledDropdown className="col-8">
-								<DropdownToggle caret/>
+								<DropdownToggle caret />
 								<DropdownMenu>
 									<DropdownItem>Item 1</DropdownItem>
 									<DropdownItem>Item 2</DropdownItem>
@@ -26,32 +26,63 @@ function NewEventCard ({cancel}) {
 						</FormGroup>
 						<FormGroup className="row">
 							<Label className="col-3"> Start</Label>
-							<Button id="cal1" className="col-4" onClick={(e)=>e.preventDefault()}> cal</Button>
-							<UncontrolledPopover trigger="legacy" placement="bottom" target="cal1">
-								<PopoverBody className="calendarpopup">
-									<Calendar />
-								</PopoverBody>
-							</UncontrolledPopover>
-							<Input className="col-4" type="text" />
+							<InputGroup className="col-4">
+								<Input />
+								<InputGroupAddon addonType="append">
+									<Button id="cal1" outline onClick={(e) => e.preventDefault()}><span className="fa fa-calendar fa-lg" /></Button>
+									<UncontrolledPopover trigger="legacy" placement="bottom" target="cal1">
+										<PopoverBody className="calendarpopup">
+											<Calendar />
+										</PopoverBody>
+									</UncontrolledPopover>
+								</InputGroupAddon>
+							</InputGroup>
+
+							<InputGroup className="col-4">
+								<Input />
+								<InputGroupAddon addonType="append">
+									<Button id="tim1" outline onClick={(e) => e.preventDefault()}><span className="fa fa-history fa-lg" /></Button>
+									<UncontrolledPopover trigger="legacy" placement="bottom" target="tim1">
+										<PopoverBody className="calendarpopup">
+											Time
+										</PopoverBody>
+									</UncontrolledPopover>
+								</InputGroupAddon>
+							</InputGroup>
 						</FormGroup>
 						<FormGroup className="row">
 							<Label className="col-3"> End</Label>
-							<Button id="cal2" className="col-4" onClick={(e) => e.preventDefault()}> cal</Button>
-							<UncontrolledPopover trigger="legacy" placement="bottom" target="cal2" >
-								<PopoverBody className="calendarpopup">
-									<Calendar />
-								</PopoverBody>
-							</UncontrolledPopover>
-							<Input className="col-4" type="text" />
+							<InputGroup className="col-4">
+								<Input />
+								<InputGroupAddon addonType="append">
+									<Button id="cal1" outline onClick={(e) => e.preventDefault()}><span className="fa fa-calendar fa-lg" /></Button>
+									<UncontrolledPopover trigger="legacy" placement="bottom" target="cal1">
+										<PopoverBody className="calendarpopup">
+											<Calendar />
+										</PopoverBody>
+									</UncontrolledPopover>
+								</InputGroupAddon>
+							</InputGroup>
+							<InputGroup className="col-4">
+								<Input />
+								<InputGroupAddon addonType="append">
+									<Button id="tim2" outline onClick={(e) => e.preventDefault()}><span className="fa fa-history fa-lg" /></Button>
+									<UncontrolledPopover trigger="legacy" placement="bottom" target="tim2">
+										<PopoverBody className="calendarpopup">
+											Time
+										</PopoverBody>
+									</UncontrolledPopover>
+								</InputGroupAddon>
+							</InputGroup>
 						</FormGroup>
 						<Row>
 							<Col>
 								<Button color="info" onClick={cancel}>
-									<span className="fa fa-plus"/> Save
+									<span className="fa fa-plus" /> Save
 								</Button>
 							</Col>
 							<Col>
-								<Button color="danger" onClick={cancel}> 
+								<Button color="danger" onClick={cancel}>
 									<span className="fa fa-trash" /> Delete
 								</Button>
 							</Col>
@@ -60,15 +91,15 @@ function NewEventCard ({cancel}) {
 									<span className="fa fa-times" /> Cancel
 								</Button>
 							</Col>
-							
-						</Row>
-						
 
-					</Form>	
-				
+						</Row>
+
+
+					</Form>
+
 				</CardBody>
 			</Card>
-			
+
 		</div>
 	)
 }
