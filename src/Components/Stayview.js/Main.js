a91fa82ac7fbf3802dc47d0acdce6cd57fb1e423
itissandeep98 <itissandeep98@gmail.com>
2020-08-13 02:04:27 +0530

    message for small screens

--- src/Components/Stayview.js/Main.js
+++ src/Components/Stayview.js/Main.js
@@ -3,19 +3,28 @@ import Header from './Header'
 import LeftNav from './LeftNav'
 import Center from './Center'
 import RightNav from './RightNav'
+import { Alert, AlertTitle } from '@material-ui/lab';
 
 export default class Main extends Component {
 	render() {
 		return (
 			<div>
-				<Header />
-				<div className="container-fluid">
-					<div className="row">
-						<LeftNav />
-						<Center />
-						<RightNav />
+				<div className="d-none d-md-block">
+					<Header />
+					<div className="container-fluid">
+						<div className="row">
+							<LeftNav />
+							<Center />
+							<RightNav />
+						</div>
 					</div>
 				</div>
+				<div className="d-md-none text-center" style={{height:"100vh"}}>
+					<Alert severity="error" style={{ height: "100vh" }}>
+						<AlertTitle>Error</AlertTitle>
+						<strong>Not available for Small Screens </strong>
+					</Alert>				
+			</div>
 			</div>
 		)
 	}
